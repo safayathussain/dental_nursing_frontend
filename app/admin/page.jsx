53e866b75d527d6c038d2803b7f7a554fcfe612a
Zahed75 <safayatofficial1@gmail.com>
2025-01-08 12:32:46 +0600

    error fix

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -2,7 +2,7 @@
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
+const Page = () => {
   const router = useRouter();
   router.push('/admin/dashboard')
   return (
@@ -10,4 +10,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
