37b0a54704f95514fa8236b0c0c67a85c9221a7a
Zahed75 <safayatofficial1@gmail.com>
2025-01-08 12:05:53 +0600

    role middleware added

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,6 +1,10 @@
+"use client"
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const page = () => {
+  const router = useRouter();
+  router.push('/admin/dashboard')
   return (
     <div></div>
   )
